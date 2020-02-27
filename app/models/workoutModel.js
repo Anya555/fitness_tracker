1f9eb9a3949b11d00907aae5d298aa4fb8683a2e
Anna Panas <annapanas0906@gmail.com>
2020-02-26 21:01:59 -0500

    added models

---
+++ app/models/workoutModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    name: {
+        type: String,
+        date: Number
+    },
+    activities: [
+        {
+            type: Schema.Ttpes.ObjectId,
+            ref: "Activities"
+        }
+    ]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+module.exports = Workout;
