6d3d75b55b74e6e6160a1e3a67ee09ca2c5b3e90
Anna Panas <annapanas0906@gmail.com>
2020-02-27 23:59:18 -0500

    able to save workouts

--- app/controllers/activityCntrl.js
+++ app/controllers/activityCntrl.js
@@ -2,7 +2,7 @@ const Activity = require("../models/activitiesModel");
 
 const ActivityCntrl = {
     // This is called (when needed) from the route page when a 
-    // listing of all exercises is needed
+    // listing of all activites is needed
     getAll(req, res){
      
       Activities.find({}).then(data => {
--- app/routes/workout.js
+++ app/routes/workout.js
@@ -6,6 +6,7 @@ const WorkoutCntrl = require('../controllers/workoutCntrl');
 // GET  "/""
 // Calls controller which will return all activities for a specific workout
 router.get("/", WorkoutCntrl.getAll);
+router.post("/", WorkoutCntrl.postWorkout);
 
 
 module.exports = router;
